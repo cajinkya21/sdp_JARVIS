ae49f4d77a5c99e00a80b5a4dcb56e0841bd7e91
cajinkya21 <cajinkya21@gmail.com>
2017-02-20 20:34:48 +0530

    ajinkya

---
+++ contactmanager/src/main/java/com/cajinkya21gmail/contactmanager/ContactManager.java
@@ -0,0 +1,13 @@
+package com.cajinkya21gmail.contactmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_manager);
+    }
+}
